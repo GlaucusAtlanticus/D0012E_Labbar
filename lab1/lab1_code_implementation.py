0113dbbffb55b973a28cbfaf0067b4da7e27ef01
jontre-0 <jontre-0@student.ltu.se>
2021-11-03 15:25:38 +0100

    added small code

--- lab1/lab1_code_implementation.py
+++ lab1/lab1_code_implementation.py
@@ -1,31 +1,35 @@
 import math
 
+
+
+### Lists ###
+
+Numbers = [5, 7, 10, 1, 4, 15, 3]
+
+
 #### Functions ###
 
 # Binary sort
-def bSort():
+def bSort(list):
   print("hej")
 
 # Splits lists into sublists, sorts them and then merges them.
-def Mergesort():
+def Mergesort(list):
   print("hej")
 
 # Linear search
-def Insertionsort():
-  print("hej")
+def Insertionsort(list):
+  
+  n = len(list)
+
+
+
 
 # Starts Sorting algorithms and prints sorted list
 def Main():
   print("hej")
 
 
-
 ### Start Main ###
 Main()
-
-
-
-
-
-
 
