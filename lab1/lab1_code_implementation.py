21a922451195f9f4ed27e08fc7959c5593ca4290
GlaucusAtlanticus <wagge00@gmail.com>
2021-11-03 15:59:11 +0100

    update code imp

--- lab1/lab1_code_implementation.py
+++ lab1/lab1_code_implementation.py
@@ -11,23 +11,34 @@ Numbers = [5, 7, 10, 1, 4, 15, 3]
 
 # Binary sort
 def bSort(list):
-  print("hej")
+    print("hej")
 
 # Splits lists into sublists, sorts them and then merges them.
-def Mergesort(list):
-  print("hej")
+def MergesortLine(list):
+    print("hej")
+
+# Splits lists into sublists, sorts them and then merges them.
+def MergesortBin(list):
+    # tolkade labspecen som att en mergsort ska hantera linjärt och en som hanterar bin
+    print("hej")
 
 # Linear search
-def Insertionsort(list):
-  
-  n = len(list)
+def Insertionsort(list):    
+    n = len(list)
 
-
-
+# Linear Plain text
+# Vårat input är en (osorterad) lista av N element 
+# Vårat Output är en (sorterad) lista av N element
+# 
+# Hur funktionen funkar
+# Funktionen funkar genom att man tar element (i)
+# Jämför det elementet (j) som är den del av listan
+#   ifall i < j 
+##### More to come ######
 
 # Starts Sorting algorithms and prints sorted list
 def Main():
-  print("hej")
+    print("hej")
 
 
 ### Start Main ###
