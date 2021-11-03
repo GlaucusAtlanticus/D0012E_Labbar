1f66745a0ff952a26803a47b6534056f75be9567
jontre-0 <jontre-0@student.ltu.se>
2021-11-03 14:51:48 +0100

    added some comments in lab1 code

--- lab1/lab1_code_implementation.py
+++ lab1/lab1_code_implementation.py
@@ -1,18 +1,22 @@
 import math
 
 #### Functions ###
+
+# Binary sort
 def bSort():
+  print("hej")
 
+# Splits lists into sublists, sorts them and then merges them.
 def Mergesort():
+  print("hej")
 
+# Linear search
 def Insertionsort():
+  print("hej")
 
+# Starts Sorting algorithms and prints sorted list
 def Main():
-
-
-
-
-
+  print("hej")
 
 
 
