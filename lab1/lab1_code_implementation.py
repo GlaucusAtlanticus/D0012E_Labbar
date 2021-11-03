138f63dde5322768937db278b2f148b15260ae1a
jontre-0 <jontre-0@student.ltu.se>
2021-11-03 15:58:21 +0100

    tab change

--- lab1/lab1_code_implementation.py
+++ lab1/lab1_code_implementation.py
@@ -11,23 +11,23 @@ Numbers = [5, 7, 10, 1, 4, 15, 3]
 
 # Binary sort
 def bSort(list):
-  print("hej")
+    print("hej")
 
 # Splits lists into sublists, sorts them and then merges them.
 def Mergesort(list):
-  print("hej")
+    print("hej")
 
 # Linear search
 def Insertionsort(list):
   
-  n = len(list)
+    n = len(list)
 
 
 
 
 # Starts Sorting algorithms and prints sorted list
 def Main():
-  print("hej")
+    print("hej")
 
 
 ### Start Main ###
