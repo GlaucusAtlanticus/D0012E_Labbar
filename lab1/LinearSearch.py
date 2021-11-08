4ba1b09bcd2a0b673e65e9e9823eafa04ade4d18
Jonatan Trefil <jontre-0@student.ltu.se>
2021-11-08 13:38:38 +0100

    commented text for linear

--- lab1/LinearSearch.py
+++ lab1/LinearSearch.py
@@ -6,17 +6,17 @@
 # Linear search
 def Insertionsort(list):
   
-    n = len(list)
+    n = len(list)                         # Length of list
 
-    for j in range(0, n):
-        print(j)
-        while j > 0 and list[j-1] > list[j]:
+    for j in range(0, n):                 # Iterates Through the entire list
+
+        while j > 0 and list[j-1] > list[j]:        # Swaps 2 list elements if lower index element is larger.
 
             temp = list[j]                # This segment swaps j-1 and j
             list[j] = list[j-1]
             list[j-1] = temp
 
-            j = j-1
+            j = j-1                       # Lowers index value
     return list
 
 # Linear Plain text
