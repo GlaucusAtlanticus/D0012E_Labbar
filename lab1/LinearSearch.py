374d90a53a78b099f7d2b82abd6617b3de8e439b
Jonatan Trefil <jontre-0@student.ltu.se>
2021-11-12 09:50:28 +0100

    Changed 0 to 1 in for loop as the first loop with value 0 would always fail the while condition as it is not more than 0.

--- lab1/LinearSearch.py
+++ lab1/LinearSearch.py
@@ -1,5 +1,5 @@
 # Edvin Petterson
-# Jonatan Trefill
+# Jonatan Trefil
 # Rasmus Jacobsen
 
 
@@ -8,15 +8,15 @@ def Insertionsort(list):
   
     n = len(list)                         # Length of list
 
-    for j in range(0, n):                 # Iterates Through the entire list
+    for j in range(1, n):                 # Iterates Through the entire list, will make it so the left side of the for loop value is sorted.
 
-        while j > 0 and list[j-1] > list[j]:        # Swaps 2 list elements if lower index element is larger.
+        while j > 0 and list[j-1] > list[j]:        # Swaps 2 list elements if lower index element is larger. 
 
             temp = list[j]                # This segment swaps j-1 and j
             list[j] = list[j-1]
             list[j-1] = temp
 
-            j = j-1                       # Lowers index value
+            j = j-1                       # Lowers index value, ultimately towards 0 or until a smaller number is found.
     return list
 
 # Linear Plain text
