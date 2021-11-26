5b66e01be8082f46f4ec6c6a236862ca9d893a45
Jonatan Trefil <jontre-0@student.ltu.se>
2021-11-26 11:34:24 +0100

    added a gen for part 2 aswell.

--- lab2/GenerateList.py
+++ lab2/GenerateList.py
@@ -9,7 +9,7 @@ import random
 
 # Function that creates a list so that
 # size n = 3 * 2^(k-1)
-def GenList(k):
+def GenListPart1(k):
    list = []
 
    size = 3 * 2**(k-1)
@@ -17,5 +17,16 @@ def GenList(k):
    for x in range(0, size):                       # Iterates size times
       number = random.randint(-size,size)         # range of numbers
       list.append(number)                         # Adds numbers to list
+
+   return list
+
+def GenListPart2(k):
+   list = []
+
+   size = 2**k
+
+   for x in range(0, size):                       # Iterates size times
+      number = random.randint(1,size)             # range of numbers, non zero
+      list.append(number)           
 
    return list
