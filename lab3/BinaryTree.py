424a3fa70311465cc71bb3fb98df263ca1d9b230
jontre-0 <jontre-0@student.ltu.se>
2021-12-04 03:13:48 +0100

    Added more code

--- lab3/BinaryTree.py
+++ lab3/BinaryTree.py
@@ -12,8 +12,29 @@ class BinaryTree:
         root = Node(value)
 
     # Checks if a given value exists in the tree
-    def search():
-        print("temp")
+    def search(node, value):
+        if value == node.value:                     # Checks if value is in current node
+            print("Value is in Tree!")
+            return
+        elif value < node.value:                    
+            if node.left == None:                   # If element is smaller but a smaller does not exists
+                print("Value not in Tree =(")
+                return
+            else:
+                search(node.left, value)
+                return
+        else:
+            if node.right == None:                  # If element is larger but a larger does not exists
+                print("Value not in Tree =(")
+                return
+            else:
+                search(node.right, value)
+                return
+
+
+
+
+
 
     # Insert a node with a given value into a tree
     def insert():
